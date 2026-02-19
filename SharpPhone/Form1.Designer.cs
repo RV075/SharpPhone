2baac3a614c9f9c85b15b01eeccf7305fdb649c5
roxan <roxan@MSI>
2026-02-19 09:09:29 +0100

    Add project files.

---
+++ SharpPhone/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace SharpPhone
+{
+    partial class frmMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnAdd = new Button();
+            btnEdit = new Button();
+            btnDelete = new Button();
+            SuspendLayout();
+            // 
+            // btnAdd
+            // 
+            btnAdd.Location = new Point(667, 30);
+            btnAdd.Name = "btnAdd";
+            btnAdd.Size = new Size(100, 29);
+            btnAdd.TabIndex = 0;
+            btnAdd.Text = "+";
+            btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // btnEdit
+            // 
+            btnEdit.Location = new Point(667, 65);
+            btnEdit.Name = "btnEdit";
+            btnEdit.Size = new Size(100, 29);
+            btnEdit.TabIndex = 1;
+            btnEdit.Text = "Aanpassen";
+            btnEdit.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            btnDelete.Location = new Point(667, 100);
+            btnDelete.Name = "btnDelete";
+            btnDelete.Size = new Size(100, 29);
+            btnDelete.TabIndex = 2;
+            btnDelete.Text = "Verwijderen";
+            btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // frmMain
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnDelete);
+            Controls.Add(btnEdit);
+            Controls.Add(btnAdd);
+            Name = "frmMain";
+            Text = "SoundSharp";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button btnAdd;
+        private Button btnEdit;
+        private Button btnDelete;
+    }
+}
---
+++ SharpPhone/Form1.cs
@@ -0,0 +1,10 @@
+namespace SharpPhone
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
