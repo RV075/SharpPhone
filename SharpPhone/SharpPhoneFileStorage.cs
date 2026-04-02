c1c9bf5efe186c8395d2852d7547e2d5fc49413f
RV075 <204032@novacollege.nl>
2026-04-02 08:39:55 +0200

    json toegevoegd/werkend gemaakt

---
+++ SharpPhone/SharpPhoneFileStorage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpPhone
+{
+    public class SharpPhoneFileStorage
+    {
+        Load();
+        Save(SharpPhoneDataStore data);
+    }
+}
